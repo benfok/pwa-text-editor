1bab06e2da3bfd282c98774593667626a5bf0fc0
Ben Fok <benfok8@gmail.com>
2022-05-02 22:32:32 -0700

    updating gitignore, adding image caching, cleaning code when retrieving data from indexedDB

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,8 +1,8 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { StaleWhileRevalidate, CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
+const { CacheFirst } = require('workbox-strategies'); // strategy that hits the chache first and then goes to the server if the asset is not cached
+const { registerRoute } = require('workbox-routing'); // defines the routes upon which to use a cache. Takes in URLs and a cache strategy. Remember that URLs can also be calls to assets not just pages
+const { CacheableResponsePlugin } = require('workbox-cacheable-response'); // Used for filtering matches based on status code, header, or both
+const { ExpirationPlugin } = require('workbox-expiration'); // Used to limit entries in cache, remove entries after a certain period of time
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
@@ -24,17 +24,17 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+// Caches page navigations and then uses the pageCache callback which is setup with a cache first strategy. The call will first hit the cache, and only go to the network if the asset is not there
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 
 // Set up asset cache
 registerRoute(
-  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS, CSS and worker files)
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new StaleWhileRevalidate({
+  new CacheFirst({
     // Name of the cache storage.
-    cacheName: 'asset-cache',
+    cacheName: 'jate-asset-cache',
     plugins: [
       // This plugin will cache responses with these headers to a maximum-age of 30 days
       new CacheableResponsePlugin({
@@ -43,3 +43,37 @@ registerRoute(
     ],
   })
 );
+
+// Cache images with a Cache First strategy
+registerRoute(
+  // Check to see if the request's destination is style for an image
+  ({ request }) => request.destination === 'image',
+  // Use a Cache First caching strategy
+  new CacheFirst({
+    // Put all cached files in a cache named 'images'
+    cacheName: 'jate-image-cache',
+    plugins: [
+      // Ensure that only requests that result in a 200 status are cached
+      new CacheableResponsePlugin({
+        statuses: [200],
+      }),
+      // Don't cache more than 20 items, and expire them after 30 days
+      new ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+      }),
+    ],
+  }),
+);
+
+
+// fetch event of service worker
+self.addEventListener('fetch', event => {
+  // everytime there is a fetch request, see if what we are after is in the cache
+  event.respondWith(  
+    caches.match(event.request).then(cacheResponse => {
+      // if the asset is in the cache return it OR continue the fetch to the server
+      return cacheResponse || fetch(event.request);
+    })
+  )
+})
